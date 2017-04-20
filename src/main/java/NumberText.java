a2eb1f463e07d0767b483741e50c117834e6e4df
danil <danilaeremenko@mail.ru>
2017-04-20 13:28:50 +0300

    Поле ввода

--- src/main/java/NumberText.java
+++ src/main/java/NumberText.java
@@ -1,18 +1,23 @@
 import java.awt.*;
 import javax.swing.*;
 import javax.swing.text.*;
+
 public class NumberText extends JFrame {
-    JTextField tf = new JTextField(5);
+    JTextField tf = new JTextField(10);
     JLabel l = new JLabel("Введите цифры:");
 
     NumberText() {
         super("text");
         setLayout(new FlowLayout());
         // Вставляем фильтр вводимых символов
-        // ((PlainDocument)tf.getDocument()).         setDocumentFilter(new NumberFilter());
-        // Текст будет выделяться только красным цветом     tf.setSelectedTextColor(Color.red);
-        // При выделении текста фон останется белым     tf.setSelectionColor(Color.white);
-        // Курсор будет красным     tf.setCaretColor(Color.red);     l.setLabelFor(tf);
+        ((PlainDocument) tf.getDocument()).setDocumentFilter(new NumberFilter());
+        // Текст будет выделяться только красным цветом
+        tf.setSelectedTextColor(Color.red);
+        // При выделении текста фона станет черным
+        tf.setSelectionColor(Color.black);
+        // Курсор будет белым
+        tf.setCaretColor(Color.white);
+        l.setLabelFor(tf);
         add(l);
         add(tf);
         setSize(400, 400);
@@ -25,16 +30,25 @@ public class NumberText extends JFrame {
         System.out.println();
 
     }
+
 }
+
+
 // Фильтр вводимых данных
-// class NumberFilter extends DocumentFilter{
-// Переопределяем только один метод     public void insertString(FilterBypass fb, int pos,String text, AttributeSet attr)
-// throws BadLocationException
-// {      try{Integer.parseInt(text);
+class NumberFilter extends DocumentFilter {
+    // Переопределяем только один метод
+    public void insertString(FilterBypass fb, int pos, String text, AttributeSet attr)
+            throws BadLocationException {
+        try {
+            Integer.parseInt(text);
 // Введена цифра?
-// }catch(Exception e)
-// {
+        } catch (Exception e) {
 // Если не цифра, то символ не вводим
-// super.insertString(fb, 0, "", attr);
-// return;      }
-// Если введена цифра, то заносим ее в поле      super.insertString(fb, pos, text, attr);      }    } }
+            super.insertString(fb, 0, "", attr);
+            return;
+        }
+        //Если введена цифра, то заносим ее в поле
+        super.insertString(fb, pos, text, attr);
+
+    }
+}
